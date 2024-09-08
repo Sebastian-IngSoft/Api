ebf13d895b6950165468edcfde8aab5b4f2b54a1
Sebastian Sarmiento PC <sebastiansarmientocas@gmail.com>
2024-09-07 19:45:54 -0500

    clase 5 registro de usuarios

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [RegisterController::class,'store'])->name('api.v1.register'); 
